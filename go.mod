66422c136943ccd0f8a2a3c7e3bb663633544a79
Maksim Kuprienko <maksim.kuprienko@flant.com>
2021-10-14 12:27:39 +0300

    Initial commit

---
+++ go.mod
@@ -0,0 +1,16 @@
+module exporter-index-size-today
+
+go 1.16
+
+require (
+	github.com/alecthomas/units v0.0.0-20210927113745-59d0afb8317a // indirect
+	github.com/golang/protobuf v1.5.2 // indirect
+	github.com/google/go-cmp v0.5.6 // indirect
+	github.com/matttproud/golang_protobuf_extensions v1.0.2-0.20181231171920-c182affec369 // indirect
+	github.com/prometheus/client_golang v1.11.0
+	github.com/stretchr/testify v1.7.0 // indirect
+	golang.org/x/sys v0.0.0-20210603125802-9665404d3644 // indirect
+	golang.org/x/xerrors v0.0.0-20200804184101-5ec99f83aff1 // indirect
+	gopkg.in/alecthomas/kingpin.v2 v2.2.6
+	gopkg.in/yaml.v3 v3.0.0-20210107192922-496545a6307b // indirect
+)
---
+++ main.go
@@ -0,0 +1,156 @@
+package main
+
+import (
+	"crypto/tls"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"strconv"
+	"regexp"
+	"time"
+	"log"
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+	"gopkg.in/alecthomas/kingpin.v2"
+)
+
+const namespace = "oneday_elasticsearch"
+
+var (
+	pattern_digital = "^[[:digit:]]"
+	clusterName = kingpin.Flag("cluster", "Cluster's name").Default("opendistro").String()
+	re = regexp.MustCompile("[0-9]+")
+	data []Index
+	currentDate = time.Now()
+	metricsPath = kingpin.Flag("path", "URL path for collected metrics").Default("/metrics").String()
+	listenPort = kingpin.Flag("port", "Export's port").Default(":9101").String()
+	esUrl = kingpin.Flag("esUrl", "ElasticSearch's URL").Default("https://opendistro").String()
+	esPort = kingpin.Flag("esPort", "ElasticSearch's port").Default("9200").String()
+	label = []string{"index", "cluster"}
+	indexSize = prometheus.NewDesc(
+		prometheus.BuildFQName(namespace, "indices_store", "size_bytes_primary"), "Size for each index in today", label, nil,
+	)
+	docsCount = prometheus.NewDesc(
+		prometheus.BuildFQName(namespace, "indices_docs", "total"), "Total count off docs in index", label, nil,
+	)
+)
+
+type Collector struct{}
+
+func (i *Collector) Describe(ch chan<- *prometheus.Desc) {
+	ch <- indexSize
+	ch <- docsCount
+}
+
+
+type Index struct {
+	Name    string `json:"index"`
+	Docs 	int `json:"dc"`
+	Size    string `json:"pri.store.size"`
+}
+
+func match(pattern string, text string) string {
+    matched, _ := regexp.Match(pattern, []byte(text))
+	var number = "digital"
+	var str = "string"
+  if matched {
+        return number
+    } else {
+        return str
+    }
+}
+
+func healthCheck(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	_, err := fmt.Fprintln(w, `{"status":"ok"}`)
+	if err != nil {
+		log.Println("Failed to write to stream: %v", err)
+	}
+}
+
+
+func (i *Collector) Collect(ch chan<- prometheus.Metric) {
+
+	cUrl := fmt.Sprintf("%s:%s/_cat/indices/*-%s?format=json&pretty&h=index,dc,pri.store.size", *esUrl, *esPort , currentDate.Format("2006.01.02"))
+	tr := &http.Transport{
+		TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
+	}
+	client := &http.Client{Transport: tr}
+
+	req, err := http.NewRequest("GET", cUrl, nil)
+	if err != nil {
+		log.Fatal("Error! Bad request.")
+	}
+
+	resp, err := client.Do(req)
+	if err != nil {
+		log.Fatal("Error! Query doesn't return a result.")
+	}
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+	json.Unmarshal([]byte(body), &data)
+	for _, value := range data {
+		var label []string
+		get_symbol := value.Size[len(value.Size)-2:]
+		check_symbol := match(pattern_digital, get_symbol)
+		switch check_symbol {
+		case "digital":
+			originSize, _ := strconv.Atoi(re.FindAllString(value.Size, -1)[0])
+			label = append(label, value.Name, *clusterName)
+			ch <- prometheus.MustNewConstMetric(
+				indexSize, prometheus.GaugeValue, float64(originSize), label...
+			)
+		case "string":
+			measure := value.Size[len(value.Size)-2:]
+			switch measure {
+			case "gb":
+				originSize, _ := strconv.Atoi(re.FindAllString(value.Size, -1)[0])
+				newSize := originSize * 1024 * 1024 * 1024
+				label = append(label, value.Name)
+				fmt.Println("oneday_elasticsearch_indices_store_size_bytes_primary{cluster=" + *clusterName + "," + "index=" +  value.Name + "}" + " " + strconv.Itoa(newSize))
+				ch <- prometheus.MustNewConstMetric(
+					indexSize, prometheus.GaugeValue, float64(newSize), label...
+				)
+			case "mb":
+				originSize, _ := strconv.Atoi(re.FindAllString(value.Size, -1)[0])
+				newSize := originSize * 1024 * 1024
+				fmt.Println("oneday_elasticsearch_indices_store_size_bytes_primary{cluster=" + *clusterName + "," + "index=" +  value.Name + "}" + " " + strconv.Itoa(newSize))
+			case "kb":
+				originSize, _ := strconv.Atoi(re.FindAllString(value.Size, -1)[0])
+				newSize := originSize * 1024
+				fmt.Println("oneday_elasticsearch_indices_store_size_bytes_primary{cluster=" + *clusterName + "," + "index=" +  value.Name + "}" + " " + strconv.Itoa(newSize))
+			}
+		}
+		ch <- prometheus.MustNewConstMetric(
+			docsCount, prometheus.GaugeValue, float64(value.Docs), label...
+		)
+	}
+}
+
+func main(){
+	kingpin.Version("0.0.1")
+	kingpin.Parse()
+
+	prometheus.MustRegister(&Collector{})
+
+	http.Handle(*metricsPath, promhttp.Handler())
+	http.HandleFunc("/healthz", healthCheck)
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		_, _ = w.Write([]byte(`<html>
+<head><title>oneday-exporter</title></head>
+<body>
+<h1>oneday-exporter</h1>
+<p><a href="` + *metricsPath + `">Metrics</a></p>
+</body>
+</html>`))
+	})
+
+	log.Println("Starting oneday-exporter")
+	log.Println("Starting server on", *listenPort)
+	log.Fatal(http.ListenAndServe(*listenPort, nil))
+}
